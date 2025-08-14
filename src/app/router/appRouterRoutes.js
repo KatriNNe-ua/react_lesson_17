a08d895e201c41373efde674cac57dcb63a42fda
KatriNNe-ua <mazyrukkatya@gmail.com>
2025-08-14 17:18:05 +0300

    Update appRouterRoutes.js

--- src/app/router/appRouterRoutes.js
+++ src/app/router/appRouterRoutes.js
@@ -1,11 +1,27 @@
+// import { frontRoutes } from '@/shared/config/routes/frontRoutes'
+
+// const pagesList = Object.keys(frontRoutes.pages)
+
+// export const appRouterRoutes = pagesList.map((page) => ({
+  // ...frontRoutes.pages[page],
+  // lazy: async () => ({
+    // Component: (await import(`../../pages/${page}.jsx`)).default,
+  // }),
+// }))
+// src/app/router/appRouterRoutes.js
 import { frontRoutes } from '@/shared/config/routes/frontRoutes'
+
+// Створюємо мапу всіх сторінок
+const pagesModules = import.meta.glob('../../pages/*.jsx', { eager: false })
 
 const pagesList = Object.keys(frontRoutes.pages)
 
 export const appRouterRoutes = pagesList.map((page) => ({
   ...frontRoutes.pages[page],
-  lazy: async () => ({
-    Component: (await import(`../../pages/${page}.jsx`)).default,
-  }),
+  lazy: async () => {
+    const mod = await pagesModules[`../../pages/${page}.jsx`]() // ← строго за ключем
+    return { Component: mod.default }
+  },
 }))
+
 
